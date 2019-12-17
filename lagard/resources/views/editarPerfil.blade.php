b99cf55add1b7a16d43cd75503b1af554d225b1f
leandrosobrino <sobrinoleandro01@gmail.com>
2019-12-17 18:07:49 -0300

    vista de editar perfil

--- lagard/resources/views/editarPerfil.blade.php
+++ lagard/resources/views/editarPerfil.blade.php
@@ -3,7 +3,17 @@
 @extends('layouts.app')
 <div class="contenedorEditarPerfil">
     <div class="contenedorDeDatosEP">
+        <div class="fotoYNombre">
+            <div class="fotoEP"><img class="estaEsLaFoto" src="../img/sinFoto.jpg" alt="perfil"></div>
+            <div class="nombreEP"><h1 class="hNombreUsr">Nombre Completo</h1></div></div>
+        
 
+        <div class="infoYTitulo">
+            <div class="tituloEP"></div>
+            <div class="informacionEP"></div>
+        </div>
 
+      
     </div>
 </div>
+@endsection
