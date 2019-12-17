f5491d4b527eb0924657d74a6ef762bb0e00a7d7
leandrosobrino <sobrinoleandro01@gmail.com>
2019-12-17 20:15:36 -0300

    casi terminado el editar perfil

--- lagard/resources/views/editarPerfil.blade.php
+++ lagard/resources/views/editarPerfil.blade.php
@@ -12,8 +12,26 @@
         <div class="infoYTitulo">
             <div class="tituloEP"> <h1 class="editarPerfilEP"> Editar Perfil</h1></div>
             <div class="informacionEP">
-                <div class="tituloInfoEP"> Informcion Personal:</div>
+                <div class="tituloInfoEP"> <h1 class="infoPerfilEditar">Informcion Personal:</h1>
+                <div class="infomacionParaEditar">
+                    <ul >
+                    <li> <label for=""> Nombre Usuario</label> <input type="text"  readonly name="user" value="{{$user->username}}" ></li>
 
+                    <li> <label for=""></label>Nombre Completo <input type="text"  name="nombre"  value="{{$user->name}}"></li>
+
+                    <li> <label for=""> Email:</label> <input type="text" name="email"  value="{{$user->email}}"></li>
+
+                    <li> <label for=""> Contraseña:</label> <input type="text" name="password" ></li>
+
+                    <li> <label for="">Foto </label> <input type="file" name="foto"></li>
+                </ul>
+                <div class="botonesEditarPerfil"> 
+<input type="submit" value="Guardar" > {{csrf_field()}}
+<input type="button" value="Volver">
+
+                </div>
+                </div></div>
+                
             </div>
         </div>
 
