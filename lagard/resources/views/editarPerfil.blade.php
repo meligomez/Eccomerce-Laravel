1ece1b2bb24d4dd52ea565201edfce9d09d8eb47
leandrosobrino <sobrinoleandro01@gmail.com>
2019-12-18 15:40:31 -0300

    algunos cambios de css y ahora pruebo el carrito

--- lagard/resources/views/editarPerfil.blade.php
+++ lagard/resources/views/editarPerfil.blade.php
@@ -10,20 +10,20 @@
         
 
         <div class="infoYTitulo">
-            <div class="tituloEP"> <h1 class="editarPerfilEP"> Editar Perfil</h1></div>
+            <div class="tituloEP"> <h1 class="editarPerfilEP"> Editar Perfil :</h1></div>
             <div class="informacionEP">
-                <div class="tituloInfoEP"> <h1 class="infoPerfilEditar">Informcion Personal:</h1>
+                <div class="tituloInfoEP"> <h1 class="infoPerfilEditar">Informcion Personal :</h1>
                 <div class="infomacionParaEditar">
                     <ul >
-                    <li> <label for=""> Nombre Usuario</label> <input type="text"  readonly name="user" value="{{$user->username}}" ></li>
+                    <li class="liInformacionEspacio"> <label for="">- Nombre Usuario :</label> <input type="text"  readonly name="user" value="{{$user->username}}" ></li>
 
-                    <li> <label for=""></label>Nombre Completo <input type="text"  name="nombre"  value="{{$user->name}}"></li>
+                    <li class="liInformacionEspacio"> <label for=""></label>- Nombre Completo :<input type="text"  name="nombre"  value="{{$user->name}}"></li>
 
-                    <li> <label for=""> Email:</label> <input type="text" name="email"  value="{{$user->email}}"></li>
+                    <li class="liInformacionEspacio"> <label for="">- Email :</label> <input type="text" name="email"  value="{{$user->email}}"></li>
 
-                    <li> <label for=""> Contraseña:</label> <input type="text" name="password" ></li>
+                    <li class="liInformacionEspacio"> <label for="">- Contraseña :</label> <input type="text" name="password" ></li>
 
-                    <li> <label for="">Foto </label> <input type="file" name="foto"></li>
+                    <li class="liInformacionEspacio"> <label for="">- Foto : </label> <input type="file" name="foto"></li>
                 </ul>
                 <div class="botonesEditarPerfil"> 
 <input type="submit" value="Guardar" > {{csrf_field()}}
